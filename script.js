253c339b582539a05a1f479f4dc4dd2914cb6a24
Juan Sebastian Calvo <jscalvo5@misena.edu.co>
2021-10-07 19:08:32 -0500

    trate de verias formas pero solo logro mostrar el nombre

---
+++ script.js
@@ -0,0 +1,13 @@
+let socialMedia = [
+    facebook = 'http://facebook.com/viewsource',
+    twitter = 'http://twitter.com/planetoftheweb',
+    flickr = 'http://flickr.com/planetotheweb',
+    youtube = 'http://youtube.com/planetoftheweb'
+];
+document.getElementById("facebook").src = "../images/facebook.png";
+document.getElementById("google").src = "../images/facebook.png";
+document.getElementById("linkendin").src = "../images/facebook.png";
+document.getElementById("wordpress").src = "../images/facebook.png";
+document.getElementById("youtube").src = "../images/facebook.png";
+document.getElementById("twiter").src = "../images/facebook.png";
+document.getElementById("facebook").src = "../images/facebook.png";
